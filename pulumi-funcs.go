910a4b4c28fdfb4cf6fdb55b3b15ca3031eca6a0
Ameer Deen <writeameer@gmail.com>
2022-06-25 12:37:37 +0400

    Returning err vs. exit

--- pulumi-funcs.go
+++ pulumi-funcs.go
@@ -20,7 +20,7 @@ type PulumiRunParameters struct {
 	PulumiFn    pulumi.RunFunc      // Your pulumi program you want to run, passed in as a function
 }
 
-func RunPulumi(ctx context.Context, params *PulumiRunParameters) {
+func RunPulumi(ctx context.Context, params *PulumiRunParameters) error {
 
 	// Get run params
 	//orgName := params.OrgName
@@ -49,8 +49,11 @@ func RunPulumi(ctx context.Context, params *PulumiRunParameters) {
 		err = w.InstallPlugin(ctx, "azure-native", "v1.64.1")
 		if err != nil {
 			fmt.Printf("Failed to install program plugins: %v\n", err)
-			os.Exit(1)
+			//os.Exit(1)
+			return err
 		}
+
+		return nil
 	}
 
 	// set stack configuration specifying the AWS region to deploy
@@ -62,7 +65,8 @@ func RunPulumi(ctx context.Context, params *PulumiRunParameters) {
 	_, err = s.Refresh(ctx)
 	if err != nil {
 		fmt.Printf("Failed to refresh stack: %v\n", err)
-		os.Exit(1)
+		//os.Exit(1)
+		return err
 	}
 
 	fmt.Println("Refresh succeeded!")
@@ -77,9 +81,10 @@ func RunPulumi(ctx context.Context, params *PulumiRunParameters) {
 		_, err := s.Destroy(ctx, stdoutStreamer)
 		if err != nil {
 			fmt.Printf("Failed to destroy stack: %v", err)
+			return err
 		}
 		fmt.Println("Stack successfully destroyed")
-		os.Exit(0)
+		return nil
 	}
 
 	fmt.Println("Starting update")
@@ -91,9 +96,10 @@ func RunPulumi(ctx context.Context, params *PulumiRunParameters) {
 	_, err = s.Up(ctx, stdoutStreamer)
 	if err != nil {
 		fmt.Printf("Failed to update stack: %v\n\n", err)
-		os.Exit(1)
+		return err
 	}
 
 	fmt.Println("Update succeeded!")
 
+	return nil
 }
